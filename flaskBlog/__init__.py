5e32aac2e0ea6dbf55fb26666be0bbcaaab5bca3
fahadaleem <faleem396@gmail.com>
2021-06-11 01:04:39 +0500

    commit

--- flaskBlog/__init__.py
+++ flaskBlog/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
 app = Flask(__name__)
+
+
+app.config.from_object(Config)
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
--- flaskBlog/models.py
+++ flaskBlog/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userName = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(40), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
     # posts = db.relationship('Post', backref='author', lazy=True)
     def __repr__(self):
         return f'User({self.userName}, {self.email})'
@@ -19,6 +19,5 @@ class Post(db.Model):
     tile = db.Column(db.String(20), unique=True, nullable=False)
     description = db.Column(db.Text, nullable=False)
     postedDate = db.Column(db.DateTime, default=datetime.utcnow)
-    author = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     def __repr__(self):
         return f'Post({self.title}, {self.description})'        
