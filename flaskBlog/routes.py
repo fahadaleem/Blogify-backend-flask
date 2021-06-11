13ecf98068a5b49b02407fc8ad5226487d8c1946
fahadaleem <faleem396@gmail.com>
2021-06-11 15:36:49 +0500

    Post api added

--- flaskBlog/routes.py
+++ flaskBlog/routes.py
@@ -5,6 +5,14 @@ from flaskBlog.models import Post
 
 db.create_all()
 
+def convert_json_to_list(x):
+    return {
+        "id":f'{x.id}',
+        "title":f'{x.title}',
+        "description":f'{x.description}',
+        "posted_date":f'{x.postedDate}'
+    }
+
 
 @app.route("/")
 def home():
@@ -13,6 +21,10 @@ def home():
 @app.route("/get-all-post", methods=['GET'])
 def get_all_posts():
     all_posts = Post.query.all()
+
+    all_posts = list(map(convert_json_to_list, all_posts))
+
+
     json = {
         "total_posts" : f'{len(all_posts)}',
         "posts" : f'{all_posts}'
