735bdacde8cdc654ec71cd36675eadf7b7db3cfa
fahadaleem <faleem396@gmail.com>
2021-06-11 15:29:05 +0500

    Post api added

--- flaskBlog/models.py
+++ flaskBlog/models.py
@@ -4,19 +4,19 @@ from sqlalchemy.orm import backref, defaultload
 from flaskBlog import db
 from datetime import datetime
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    userName = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(40), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    # posts = db.relationship('Post', backref='author', lazy=True)
-    def __repr__(self):
-        return f'User({self.userName}, {self.email})'
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     userName = db.Column(db.String(20), unique=True, nullable=False)
+#     email = db.Column(db.String(40), unique=True, nullable=False)
+#     password = db.Column(db.String(120), nullable=False)
+#     # posts = db.relationship('Post', backref='author', lazy=True)
+#     def __repr__(self):
+#         return f'User({self.userName}, {self.email})'
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    tile = db.Column(db.String(20), unique=True, nullable=False)
+    title = db.Column(db.String(20), unique=True, nullable=False)
     description = db.Column(db.Text, nullable=False)
     postedDate = db.Column(db.DateTime, default=datetime.utcnow)
     def __repr__(self):
--- flaskBlog/routes.py
+++ flaskBlog/routes.py
@@ -1,28 +1,24 @@
 from flaskBlog import app
 from flask import json, render_template, jsonify, request
 from flaskBlog import db
-from flaskBlog.models import User
+from flaskBlog.models import Post
+
+db.create_all()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return 'Server Started'
 
-@app.route("/about")
-def about():
-    return render_template('about.html')    
+@app.route("/get-all-post", methods=['GET'])
+def get_all_posts():
+    all_posts = Post.query.all()
+    json = {
+        "total_posts" : f'{len(all_posts)}',
+        "posts" : f'{all_posts}'
+    }
+    return jsonify(json)
+    
+ 
 
-
-@app.route("/get-all-users", methods=["GET"])
-def get_all_users():
-    users = User.query.all() 
-    return {
-        "users":f'{users[0].userName}'
-    }    
-
-
-@app.route("/post-new-data", methods=["POST"])
-def post_data():
-    print(User.query.all())
-    return 'data accepted'
 
